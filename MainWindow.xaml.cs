0c1d48021224baa4552080a93baa67ea6a2cb1e7
AHJS93 <123779946+AHJS93@users.noreply.github.com>
2025-03-13 17:48:11 -0500

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System.IO;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace cardGame1;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+
+    private void dealBtn_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+    {
+        slot1.Source = null;
+        slot2.Source = null;
+        slot3.Source = null;
+        slot4.Source = null;
+        slot5.Source = null;
+        string[] cards = Directory.GetFiles("/images/cards/", "*.png");
+        Random rng = new Random();
+        int index = rng.Next(cards.Length);
+    }
+}
